78b2d17e686b053138989ffa217f5a0a4887d270
Felipe Fagundes <felipe.fagundes@catolicasc.org.br>
2020-08-24 09:27:58 -0300

    Primeiro Programa Java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta e uma saudacao: ");
+		System.out.println("Hello, world!");
+		System.out.printf("Idade: %d\n", 20);	
+		}
+	  
+}
+
+
+// javac Hello.java - compila para bytecode (.class)
+// java Hello - executa o arquico (.class)
